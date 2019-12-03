8b494044bcb173d81aa70fe5b4eaf597b86ffa67
[gayathri buddhika] <[wg.gayathri@gmail.com]>
2019-12-03 19:18:13 +0530

    Updated

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/sabra.jpg" class="logo"><br><br>
+    
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 10px;
+    width: 99%;
+    height: 255px;
+    background-color: #faf0e6;
+    position: fixed;
+    box-shadow: 0.1px 0.1px 15px 0.1px #273c75;
+    border-radius:10px;
+}
+
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>Faculty of Applied Sciences</h1>
+    <h2>Inventory Management System</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Sabaragamuwa University of Sri Lanka'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
