ccb0b4b916965139d53f181c33fd97c27b91da1c
gayathribuddhika <wg.gayathri@gmail.com>
2019-12-13 21:53:43 +0530

    updated app.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
-<nav class = "navbar navbar-default">
+
+<nav class = "navbar navbar-light bg-primary">
   <div class = "container-fluid">
     <div id = "app">
       <div class="row">
         <div class="col-sm-2">
           <img src = "./assets/sabra.jpg">
         </div>
-        <div class="col-sm-10">
+        <div class="col-sm-8">
           <h1>{{ msg }}</h1>
           <h3>Faculty of Applied Sciences</h3>
           <h3>Sabaragamuwa University of sri Lanka</h3>
+        </div>
+        <div class="col-sm-2" style="padding-top:65px">
+          <button type="button" class="btn btn-info" style="display: block; text-align:right; margin:auto;">Login</button> 
+        </div>
+      
       </div>
-      </div>
-    </div>
     <router-view/>
-  </div>  
-  
-</nav>  
+    </div> 
+  </div> 
+
+</nav> 
+
 </template>
 
 <script>
@@ -24,8 +30,13 @@ export default {
   name: 'App',
   data(){
     return{
-      msg:'Inventory Management System'
+      msg:'Inventory Management System',
+      button:"Login"
+            
+
     }
+    
+
   }
   
 }
@@ -33,8 +44,6 @@ export default {
 
 
 <style>
-#app {
-  text-align: left;
 
-}
+
 </style>
