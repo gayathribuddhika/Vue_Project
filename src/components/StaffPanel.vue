7322f5991ff9a1202eb088704c70cf37f8b80a05
gayathribuddhika <wg.gayathri@gmail.com>
2020-01-05 18:31:13 +0530

    changed

--- src/components/StaffPanel.vue
+++ src/components/StaffPanel.vue
@@ -1,18 +1,30 @@
 <template>
-    <div id="staffpanel">
-        <h1><center>Staff Panel</center></h1>
-    </div>
-</template>
-
-<script>
-export default {
+<div>
+  <b-card
     
-}
-</script>
+    style="max-width: 20rem;"
+    img-src="https://placekitten.com/380/200"
+    img-alt="Image"
+    img-top
+    body-bg-variant="dark"
+    body-text-variant="white"
+  >
+    <b-card-body>
+      <b-card-title>Admin Name</b-card-title>
+    </b-card-body>
 
-<style>
-#staffpanel{
-    padding-top: 30px;
-    padding-bottom: 300px;
-}
-</style>
+    <b-list-group variant="dark">
+      <b-list-group-item variant="dark" href="#">Dashboard</b-list-group-item>
+      <b-list-group-item variant="dark" href="#">View Current Status</b-list-group-item>
+      <b-list-group-item variant="dark" href="#">Edit Profile</b-list-group-item>
+      <b-list-group-item variant="dark" href="#">Change Password</b-list-group-item>
+      <b-list-group-item variant="dark" href="#">Add New Record</b-list-group-item>
+      <b-list-group-item variant="dark" href="#">Delete a Record</b-list-group-item>
+      <b-list-group-item variant="dark" href="#">Create a Report</b-list-group-item>
+      <b-list-group-item variant="dark" href="#">Logout</b-list-group-item>
+    </b-list-group>
+
+    
+  </b-card>
+</div>
+</template>
