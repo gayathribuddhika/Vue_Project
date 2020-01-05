e3d68691f2f0a7ec1c94b4931ac0ab456ed4be67
gayathribuddhika <wg.gayathri@gmail.com>
2020-01-05 15:57:22 +0530

    updated

--- src/main.js
+++ src/main.js
@@ -3,15 +3,20 @@ import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import LayoutPlugin from 'bootstrap-vue'
+import NavbarPlugin from 'bootstrap-vue'
+
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(LayoutPlugin)
+Vue.use(NavbarPlugin)
+
 
 new Vue({
   el: '#app',
