c2b9d76ae9422bd7a8393e81d1e614699238fd74
gayathribuddhika <wg.gayathri@gmail.com>
2020-01-10 22:33:28 +0530

    changed

--- src/components/AdminPanel.vue
+++ src/components/AdminPanel.vue
@@ -1,27 +1,26 @@
 <template>
 <div>
+  
   <b-card
-    
     style="max-width: 20rem;"
-    img-src="https://placekitten.com/380/200"
     img-alt="Image"
     img-top
     body-bg-variant="dark"
     body-text-variant="white"
   >
+  <center><b-img :src="require('../assets/sabra.png')"></b-img></center>
     <b-card-body>
-      <b-card-title>Admin Name</b-card-title>
+      <b-card-title><center>Admin Name</center></b-card-title>
     </b-card-body>
 
     <b-list-group variant="dark">
       <b-list-group-item variant="dark" href="#">Dashboard</b-list-group-item>
       <b-list-group-item variant="dark" href="#">View Current Status</b-list-group-item>
-      <b-list-group-item variant="dark" href="#">Edit Profile</b-list-group-item>
-      <b-list-group-item variant="dark" href="#">Change Password</b-list-group-item>
       <b-list-group-item variant="dark" href="#">Add New Record</b-list-group-item>
       <b-list-group-item variant="dark" href="#">Delete a Record</b-list-group-item>
       <b-list-group-item variant="dark" href="#">Create a Report</b-list-group-item>
-      <b-list-group-item variant="dark" href="#">Logout</b-list-group-item>
+      <b-list-group-item variant="dark" href="#">Edit Profile</b-list-group-item>
+      <b-list-group-item variant="dark" href="home.vue">Logout</b-list-group-item>
     </b-list-group>
     </b-card>
 </div>
