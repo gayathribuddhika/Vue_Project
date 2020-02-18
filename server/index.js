87b11bca1136abdf171951b2c057a6c015be5d7e
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-02-18 08:02:47 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const posts = require('./routes/api/posts');
+const posts = require('./routes/posts');
 
-app.use('/api/posts', posts);
+app.use('/posts', posts);
 
 const port = process.env.PORT || 8082;
 app.listen(port, () =>console.log(`Server started on port ${port}`));
