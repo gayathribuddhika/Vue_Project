7069a7d1e36cc43d516dd81b92db9ad9ca739586
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-03-15 16:48:17 +0530

    updated

--- Frontend/src/PostService.js
+++ Frontend/src/PostService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8082/api/posts';
+const url = 'http://localhost:8082/routes/posts';
 
 class PostService {
     //get posts
--- server/index.js
+++ server/index.js
@@ -8,9 +8,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+
 const posts = require('./routes/posts');
 
 app.use('/posts', posts);
 
 const port = process.env.PORT || 8082;
-app.listen(port, () =>console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
