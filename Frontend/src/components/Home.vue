4927d686bd1375e175fc00e5fd055d914b2561e1
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-03-16 07:53:47 +0530

    updated

--- Frontend/src/components/Home.vue
+++ Frontend/src/components/Home.vue
@@ -1,7 +1,14 @@
 <template>
-    <div id="aboutus">
-        <h1><center>Home Page</center></h1>
-    </div>
+<div id="aboutus">
+    <b-col md="6"> 
+        <h1><center>Welcome !!!</center></h1>
+        <h1><center>Faculty of Applied Sciences</center></h1>
+        <h1><center>Sabaragamuwa University of sri Lanka</center></h1>
+    </b-col>
+    <b-col md="6"> 
+        
+    </b-col>
+</div>
 </template>
 
 <script>
@@ -14,5 +21,6 @@ export default {
 #aboutus{
     padding-top: 30px;
     padding-bottom: 300px;
+    
 }
 </style>
