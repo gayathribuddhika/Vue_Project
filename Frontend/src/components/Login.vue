9cabe033b7a9c4a75a01921b870cacb1c0d30987
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-03-19 23:16:01 +0530

    updated

--- Frontend/src/components/Login.vue
+++ Frontend/src/components/Login.vue
@@ -14,49 +14,48 @@
             <b-col md="6">
               <div>
                 <b-card bg-variant="" text-variant="dark" border-variant="dark">
-              <form @submit.prevent="handleSubmit">
-                <div class="form-group">
-                  <label for="Username">Username</label>
-                  <input type="text" v-model="user1.username" id="username1" name="username" class="form-control" placeholder = "Enter Your Username" :class="{ 'is-invalid': submitted && $v.user1.username.$error }" />
-                  <div v-if="submitted && !$v.user1.username.required" class="invalid-feedback">Username is required</div>
-                </div>
-                <div class="form-group">
-                  <label for="password">Password</label>
-                  <input type="password" v-model="user1.password" id="password1" name="password" class="form-control" placeholder = "Enter Your Password" :class="{ 'is-invalid': submitted && $v.user1.password.$error }" />
-                  <div v-if="submitted && $v.user1.password.$error" class="invalid-feedback">
-                    <span v-if="!$v.user1.password.required">Password is required</span>
-                  </div>
-                </div>
-                <a href="#">Forgot your password?</a><br>
-                <div class="form-group">
-                  
-                  <button class="btn btn-primary">Login</button>
-                </div>
-              </form>
+                  <form @submit.prevent="handleSubmit">
+                    <div class="form-group">
+                      <label for="Username">Username</label>
+                      <input type="text" v-model="user1.username" id="username1" name="username" class="form-control" placeholder = "Enter Your Username" :class="{ 'is-invalid': submitted && $v.user1.username.$error }" />
+                      <div v-if="submitted && !$v.user1.username.required" class="invalid-feedback">Username is required</div>
+                    </div>
+                    <div class="form-group">
+                      <label for="password">Password</label>
+                      <input type="password" v-model="user1.password" id="password1" name="password" class="form-control" placeholder = "Enter Your Password" :class="{ 'is-invalid': submitted && $v.user1.password.$error }" />
+                      <div v-if="submitted && $v.user1.password.$error" class="invalid-feedback">
+                      <span v-if="!$v.user1.password.required">Password is required</span>
+                      </div>
+                    </div>
+                    <a href="#">Forgot your password?</a><br>
+                    <div class="form-group">
+                      <button class="btn btn-primary">Login</button>
+                    </div>
+                  </form>
                 </b-card>
               </div>
             </b-col> 
             <b-col md="6"> 
               <div>
                 <b-card bg-variant="" text-variant="dark" border-variant="dark">
-              <form @submit.prevent="handleSubmit">
-                <div class="form-group">
-                  <label for="Username">Username</label>
-                  <input type="text" v-model="user2.userName" id="username2" name="username" class="form-control" placeholder = "Enter Your Username" :class="{ 'is-invalid': submitted && $v.user2.username.$error }" />
-                  <div v-if="submitted && !$v.user2.username.required" class="invalid-feedback">Username is required</div>
-                </div>
-                <div class="form-group">
-                  <label for="password">Password</label>
-                  <input type="password" v-model="user2.password" id="password2" name="password" class="form-control" placeholder = "Enter Your Password" :class="{ 'is-invalid': submitted && $v.user2.password.$error }" />
-                  <div v-if="submitted && $v.user2.password.$error" class="invalid-feedback">
-                    <span v-if="!$v.user2.password.required">Password is required</span>
-                  </div>
-                </div>
-                <a href="#">Forgot your password?</a><br>
-                <div class="form-group">
-                  <button class="btn btn-primary">Login</button>
-                </div>
-              </form>
+                  <form @submit.prevent="handleSubmit">
+                    <div class="form-group">
+                      <label for="Username">Username</label>
+                      <input type="text" v-model="user2.userName" id="username2" name="username" class="form-control" placeholder = "Enter Your Username" :class="{ 'is-invalid': submitted && $v.user2.username.$error }" />
+                      <div v-if="submitted && !$v.user2.username.required" class="invalid-feedback">Username is required</div>
+                    </div>
+                    <div class="form-group">
+                      <label for="password">Password</label>
+                      <input type="password" v-model="user2.password" id="password2" name="password" class="form-control" placeholder = "Enter Your Password" :class="{ 'is-invalid': submitted && $v.user2.password.$error }" />
+                      <div v-if="submitted && $v.user2.password.$error" class="invalid-feedback">
+                        <span v-if="!$v.user2.password.required">Password is required</span>
+                      </div>
+                    </div>
+                    <a href="#">Forgot your password?</a><br>
+                    <div class="form-group">
+                      <button class="btn btn-primary">Login</button>
+                    </div>
+                  </form>
                 </b-card>
               </div>
             </b-col>
