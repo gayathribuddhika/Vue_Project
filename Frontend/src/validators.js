052483183d834750a09fc8ab462775cf1b4fcdc2
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-03-21 20:44:02 +0530

    changed

---
+++ Frontend/src/validators.js
@@ -0,0 +1,18 @@
+export function isUsernameadminims(value) {
+    return value === "adminims";
+  }
+  
+  /*export function notGmail(value = "") {
+    return !value.includes("gmail");
+  }
+  
+  export function isEmailAvailable(value) {
+    if (value === "") return true;
+  
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(value.length > 10);
+      }, 500);
+    });
+  }*/
+  
