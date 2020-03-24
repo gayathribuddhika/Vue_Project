d05bd4af43d18b9a19d973e1ced3fb6120530268
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-03-24 11:17:24 +0530

    updated

--- Frontend/src/components/StaffPanel.vue
+++ Frontend/src/components/StaffPanel.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+  <b-card-group>
   <b-card
     
     style="max-width: 20rem;"
@@ -15,15 +16,18 @@
 
     <b-list-group variant="dark">
       <b-list-group-item variant="dark" href="#">Dashboard</b-list-group-item>
-      <b-list-group-item variant="dark" href="#">View Current Status</b-list-group-item>
+      <b-list-group-item variant="dark"><router-link to = "currentstatus">View Current Status</router-link></b-list-group-item>
       <b-list-group-item variant="dark" href="#">Add New Record</b-list-group-item>
       <b-list-group-item variant="dark" href="#">Delete a Record</b-list-group-item>
       <b-list-group-item variant="dark" href="#">Create a Report</b-list-group-item>
       <b-list-group-item variant="dark" href="#">Edit Profile</b-list-group-item>
       <b-list-group-item variant="dark" href="home.vue">Logout</b-list-group-item>
     </b-list-group>
-
-    
   </b-card>
+  <b-card title="Dashboard">
+    <br>
+    
+    </b-card>  
+  </b-card-group>
 </div>
 </template>
