8a15ecd21e8890925c21453d077cb57dd012afd7
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-03-26 08:01:20 +0530

    updated

--- Frontend/src/components/Home.vue
+++ Frontend/src/components/Home.vue
@@ -1,9 +1,14 @@
 <template>
 <div id="homepage">
-    
+    <b-card> 
+        <b-img :src="require('../assets/lab2.jpg')"></b-img>
+      <b-card-text>
         <h1><center>Welcome !!!</center></h1>
         <h1><center>Faculty of Applied Sciences</center></h1>
         <h1><center>Sabaragamuwa University of sri Lanka</center></h1> 
+      </b-card-text>
+    </b-card>
+        
       
 </div>
 </template>
