6a6b49e0ade5474084eaf39ee86299adde693807
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-04-04 10:59:40 +0530

    changed

--- Frontend/src/components/Current_Status.vue
+++ Frontend/src/components/Current_Status.vue
@@ -32,7 +32,7 @@
                 <td>{{item.Make}}</td>
                 <td>{{item.Condition}}</td>
                 <td>{{item.Comments}}</td>
-                <td><i class="fa fa-pencil fa-lg" aria-hidden="true"></i></td>
+                <td><a href="#" onclick = "return confirm('Are you sure to delete?')">Delete</a></td>
               </tr>
             </tbody>
           </table>
