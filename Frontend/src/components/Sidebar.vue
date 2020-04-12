439fc929bbdf3382e4a361abf39426febee9216d
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-04-12 07:48:09 +0530

    changed

---
+++ Frontend/src/components/Sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+   <base-pagination :page-count="10" v-model="defaultPagination"></base-pagination>
+</template>
+
+<script>
+  export default {
+    data () {
+        return {
+          defaultPagination: 1
+        }
+      }
+  }
+</script>
