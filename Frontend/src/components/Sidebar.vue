df4a74f79401fd9bf5cb750fd7fdc5119a40215d
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-04-25 11:30:27 +0530

    changed

--- Frontend/src/components/Sidebar.vue
+++ Frontend/src/components/Sidebar.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-  <b-card body-bg-variant="light" >
+  <b-card body-bg-variant="light" border-variant="dark">
     <b-tabs pills card vertical no-body>
-      <b-tab title="Contacts" href="/contacts" active></b-tab>
-      <b-tab title="Current Details"></b-tab>
+      <b-tab title="Dashboard">
+        <b-card-text>{{dashboard}}</b-card-text>
+        
+      </b-tab>
+      <b-tab title="Current Details"><b-card-text><Contacts></Contacts></b-card-text></b-tab>
       <b-tab title="Add a new Record"><b-card-text>Tab contents 3</b-card-text></b-tab>
       <b-tab title="Delete a Record"><b-card-text>Tab contents 3</b-card-text></b-tab>
       <b-tab title="Update/Edit a Record"><b-card-text>Tab contents 3</b-card-text></b-tab>
@@ -15,11 +18,17 @@
 </template>
 
 <script>
+import Contacts from '@/components/Contacts'
 export default {
   name:"Sidebar",
   data() {
     return {
-      
+      dashboard:"Dashboard"
+    }
+  },
+  methods: {
+    login(){
+      this.$router.replace({name: "Contacts"})
     }
   },
 }
