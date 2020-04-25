1edfca8f1530f816f53d7002baecf4a93efb2213
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-04-25 12:07:25 +0530

    changed

--- Frontend/src/components/Sidebar.vue
+++ Frontend/src/components/Sidebar.vue
@@ -6,11 +6,11 @@
         <b-card-text>{{dashboard}}</b-card-text>
         
       </b-tab>
-      <b-tab title="Current Details"><b-card-text><Contacts></Contacts></b-card-text></b-tab>
-      <b-tab title="Add a new Record"><b-card-text>Tab contents 3</b-card-text></b-tab>
-      <b-tab title="Delete a Record"><b-card-text>Tab contents 3</b-card-text></b-tab>
-      <b-tab title="Update/Edit a Record"><b-card-text>Tab contents 3</b-card-text></b-tab>
-      <b-tab title="Create a Report"><b-card-text>Tab contents 3</b-card-text></b-tab>
+      <b-tab title="Current Details">{{this.contacts}}</b-tab>
+      <b-tab title="Add a new Record"><b-card-text>Add a new Record</b-card-text></b-tab>
+      <b-tab title="Delete a Record"><b-card-text>Delete a Record</b-card-text></b-tab>
+      <b-tab title="Update/Edit a Record"><b-card-text>Update/Edit a Record</b-card-text></b-tab>
+      <b-tab title="Create a Report"><b-card-text>Create a Report</b-card-text></b-tab>
       <b-tab title="Logout"></b-tab>
     </b-tabs>
   </b-card>
@@ -23,7 +23,8 @@ export default {
   name:"Sidebar",
   data() {
     return {
-      dashboard:"Dashboard"
+      dashboard:"Dashboard",
+      contacts:[]
     }
   },
   methods: {
