a21a4e01cd141023c2ba0cc9dc6b673a325ecd46
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-04-25 21:58:31 +0530

    changed

--- Frontend/src/components/Sidebar.vue
+++ Frontend/src/components/Sidebar.vue
@@ -1,34 +1,37 @@
 <template>
   <div>
-  <b-card body-bg-variant="light" border-variant="dark">
-    <b-tabs pills card vertical no-body>
-      <b-tab title="Dashboard">
-                       
-      </b-tab>
-      <b-tab title="Current Details">
-        
-      </b-tab>
-      <b-tab title="Add a new Record" ><b-card-text>Add a new Record</b-card-text></b-tab>
-      <b-tab title="Delete a Record"><b-card-text>Delete a Record</b-card-text></b-tab>
-      <b-tab title="Update/Edit a Record"><b-card-text>Update/Edit a Record</b-card-text></b-tab>
-      <b-tab title="Create a Report"><b-card-text>Create a Report</b-card-text></b-tab>
-      <b-tab title="Logout"></b-tab>
-    </b-tabs>
-  </b-card>
-</div>
+    <b-card body-bg-variant="light" border-variant="dark">
+      <b-tabs pills card vertical no-body>
+        <b-tab title="Dashboard">
+          <b-card-text>Dashboard</b-card-text>
+        </b-tab>
+        <b-tab title="Current Details">
+          <b-card-text>Current Details</b-card-text>
+        </b-tab>
+        <b-tab title="Add a new Record">
+          <b-card-text>Add a new Record</b-card-text>
+        </b-tab>
+        <b-tab title="Delete a Record">
+          <b-card-text>Delete a Record</b-card-text>
+        </b-tab>
+        <b-tab title="Update/Edit a Record">
+          <b-card-text>Update/Edit a Record</b-card-text>
+        </b-tab>
+        <b-tab title="Create a Report">
+          <b-card-text>Create a Report</b-card-text>
+        </b-tab>
+        <b-tab title="Logout"></b-tab>
+      </b-tabs>
+    </b-card>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name:"Sidebar",
+  name: "Sidebar",
   data() {
-    return {
-          
-    }
+    return {};
   },
-  methods: {
-    
-  }
+  methods: {}
 }
 </script>
