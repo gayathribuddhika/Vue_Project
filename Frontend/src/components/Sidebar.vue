572cc9e811ffe2e8c02a2ba41bb8c706b99a9f89
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-04-25 08:24:57 +0530

    updated

--- Frontend/src/components/Sidebar.vue
+++ Frontend/src/components/Sidebar.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-  <b-card >
-    <b-tabs pills card vertical variant ="dark">
-      <b-tab title="Dashboard" active><b-card-text>Tab contents 1</b-card-text></b-tab>
-      <b-tab title="Current Details"><b-card-text>Tab contents 2</b-card-text></b-tab>
+  <b-card body-bg-variant="light" >
+    <b-tabs pills card vertical no-body>
+      <b-tab title="Contacts" href="/contacts" active></b-tab>
+      <b-tab title="Current Details"></b-tab>
       <b-tab title="Add a new Record"><b-card-text>Tab contents 3</b-card-text></b-tab>
       <b-tab title="Delete a Record"><b-card-text>Tab contents 3</b-card-text></b-tab>
       <b-tab title="Update/Edit a Record"><b-card-text>Tab contents 3</b-card-text></b-tab>
       <b-tab title="Create a Report"><b-card-text>Tab contents 3</b-card-text></b-tab>
-      <b-tab title="Logout" router-link ="/login/resetpassword"></b-tab>
+      <b-tab title="Logout"></b-tab>
     </b-tabs>
   </b-card>
 </div>
@@ -16,6 +16,11 @@
 
 <script>
 export default {
-  
+  name:"Sidebar",
+  data() {
+    return {
+      
+    }
+  },
 }
 </script>
