6a62a4d5e224b91275c4453175cae8ee3d66f6a8
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-05-08 11:25:01 +0530

    changed

--- Frontend/src/components/sample.vue
+++ Frontend/src/components/sample.vue
@@ -14,36 +14,42 @@
       :items="items"
       :per-page="perPage"
       :current-page="currentPage"
-      small
       hover
-      
+      responsive="lg"
     ></b-table>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        perPage: 3,
-        currentPage: 1,
-        items: [
-          { id: 1, first_name: 'Fred', last_name: 'Flintstone' },
-          { id: 2, first_name: 'Wilma', last_name: 'Flintstone' },
-          { id: 3, first_name: 'Barney', last_name: 'Rubble' },
-          { id: 4, first_name: 'Betty', last_name: 'Rubble' },
-          { id: 5, first_name: 'Pebbles', last_name: 'Flintstone' },
-          { id: 6, first_name: 'Bamm Bamm', last_name: 'Rubble' },
-          { id: 7, first_name: 'The Great', last_name: 'Gazzoo' },
-          { id: 8, first_name: 'Rockhead', last_name: 'Slate' },
-          { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' }
-        ]
-      }
-    },
-    computed: {
-      rows() {
-        return this.items.length
-      }
+import axios from "axios";
+export default {
+  data() {
+    return {
+      perPage: 3,
+      currentPage: 1,
+      items: []
+    };
+  },
+  created() {
+    this.fetchRecord1();
+  },
+  computed: {
+    rows() {
+      return this.items.length;
+    }
+  },
+  methods: {
+    fetchRecord1() {
+      axios
+        .get("http://localhost:8085/lab/lab1")
+        .then(response => {
+          console.log(response.data);
+          this.items = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
+};
 </script>
