207a9156104d9668c606c2bc4a4a3e66b12f99f1
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-05-08 22:50:25 +0530

    changed

--- Frontend/src/components/sample.vue
+++ Frontend/src/components/sample.vue
@@ -1,19 +1,13 @@
 <template>
   <div>
     <b-card no-body>
-      <br>
+      <br />
       <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-          <h2>Current Details of Items</h2>
-          <form class="form-inline">
-            <input
-              id="itemInput"
-              class="form-control mr-sm-2"
-              type="text"
-              placeholder="Search"
-              
-            />
-            <!--<button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>-->
-          </form>
+        <h2>Current Details of Items</h2>
+        <form class="form-inline">
+          <input id="itemInput" class="form-control mr-sm-2" type="text" placeholder="Search" />
+          <!--<button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>-->
+        </form>
       </nav>
       <br />
       <b-tabs pills card width="100%" content-class="mt-3" justified>
@@ -24,63 +18,54 @@
           <br />
           <div class="overflow-auto">
             <b-pagination
-      v-model="currentPage"
-      :total-rows="rowtable1"
-      :per-page="perPage"
-      aria-controls="my-table"
-    ></b-pagination>
-    <p class="mt-3">Current Page: {{ currentPage }}</p>
-          <b-table
-            id="my-table"
-            :items="itemsLab1"
-            :fields="fields"
-            :per-page="perPage"
-            :current-page="currentPage"
-            hover
-            striped
-            bordered
-            responsive="lg"
-          ></b-table>
+              v-model="currentPage"
+              :total-rows="rowtable1"
+              :per-page="perPage"
+              aria-controls="my-table"
+            ></b-pagination>
+            <p class="mt-3">Current Page: {{ currentPage }}</p>
+            <b-table
+              id="my-table"
+              :items="itemsLab1"
+              :fields="fields"
+              :per-page="perPage"
+              :current-page="currentPage"
+              hover
+              striped
+              bordered
+              responsive="lg"
+            ></b-table>
           </div>
         </b-tab>
-        <b-tab title="CIS/LAB/02">
+        <!--<b-tab title="CIS/LAB/02">
           <center>
             <h3>CIS/LAB/02</h3>
           </center>
           <br />
           <div class="overflow-auto">
             <b-pagination
-      v-model="currentPage"
-      :total-rows="rowtable2"
-      :per-page="perPage"
-      aria-controls="my-table"
-    ></b-pagination>
-    <p class="mt-3">Current Page: {{ currentPage }}</p>
-          <b-table
-            id="my-table"
-            :items="itemsLab2"
-            :fields="fields"
-            :per-page="perPage"
-            :current-page="currentPage"
-            table-variant="primary"
-            hover
-            striped
-            bordered
-            responsive="lg"
-          >
-          <template>
-        <b-button size="sm" class="mr-1">
-          Info modal
-        </b-button>
-        <b-button size="sm" >
-          Details
-        </b-button>
-      </template>
-
-          </b-table>
-
+              v-model="currentPage"
+              :total-rows="rowtable2"
+              :per-page="perPage"
+              aria-controls="my-table"
+            ></b-pagination>
+            <p class="mt-3">Current Page: {{ currentPage }}</p>
+            <b-table
+              id="my-table"
+              :items="itemsLab2"
+              :fields="fields"
+              :per-page="perPage"
+              :current-page="currentPage"
+              table-variant="primary"
+              hover
+              striped
+              bordered
+              responsive="lg"
+            >
+              
+            </b-table>
           </div>
-        </b-tab>
+        </b-tab>-->
       </b-tabs>
     </b-card>
   </div>
@@ -91,10 +76,20 @@ import axios from "axios";
 export default {
   data() {
     return {
-      fields:["ID", "Main_Category", "Asset_Description", "Serial_Num", "Asset_Code", "Qty", "Make", "Condition", "Comments"],
+      fields: [
+        "ID",
+        "Main_Category",
+        "Asset_Description",
+        "Serial_Num",
+        "Asset_Code",
+        "Qty",
+        "Make",
+        "Condition",
+        "Comments"
+      ],
       itemsLab1: [],
       itemsLab2: [],
-      
+
       perPage: 3,
       currentPage: 1
     };
@@ -139,5 +134,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
