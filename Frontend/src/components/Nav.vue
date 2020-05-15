a4b77922b90e345312377a998951d02310fdc6c3
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-05-15 19:16:22 +0530

    changed

--- Frontend/src/components/Nav.vue
+++ Frontend/src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
     <div id ="links">
-        <b-nav tabs>
+        <b-nav tabs >
             <b-nav-item><router-link to = '/'>Home</router-link></b-nav-item>
             <b-nav-item><router-link to = '/contacts'>Contacts</router-link></b-nav-item>
             <b-nav-item><router-link to = '/aboutus'>About Us</router-link></b-nav-item>
