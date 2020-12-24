3abfb1c9beba6574a8214facb072dd7d8b19d6a8
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-12-24 22:51:45 +0530

    fixed issue in login

--- Frontend/src/components/Login.vue
+++ Frontend/src/components/Login.vue
@@ -1,87 +1,56 @@
 <template>
   <div id="form">
-    <!-- <b-jumbotron bg-variant="light" text-variant="dark" border-variant="dark"> -->
-      <b-row>
-        <b-col  md="6">
-          <h2>
-            <center>Admin Login</center>
-          </h2>
-        </b-col>
-        <!-- <b-col md="6">
-          <h2>
-            <center>Staff Login</center>
-          </h2>
-        </b-col> -->
-      </b-row>
-      <b-container fluid>
-        <b-row>
-          <b-col md="6">
-            <div>
-              <b-card bg-variant text-variant="dark" border-variant="dark">
-                <b-form @submit.prevent="login">
-                  <center><p id="msg" v-if="submit_login === 'error'">{{err_msg}}</p></center>
-                  <b-form-group label="Username:" label-for="username">
-                    <b-form-input
-                      id="username"
-                      placeholder="Enter your username"
-                      required
-                      v-model="login_form.username"
-                      
-                    />
-                  </b-form-group>
+    <h2>
+      <center>Admin Login</center>
+    </h2>
 
-                  <b-form-group label="Password:" label-for="password">
-                    <b-form-input
-                      id="password"
-                      type="password"
-                      placeholder="Enter your password"
-                      required
-                      v-model="login_form.password"
-                      
-                    />
-                    <br />
-                    <router-link to="/login/resetpassword">Forgot your password?</router-link>
-                  </b-form-group>
-                  <b-button type="submit" variant="success" :disabled="submit_login === 'ok'">Login</b-button>
-                  <!-- <center><p>Not Admin <router-link to="#">Login here</router-link></p></center> -->
-                </b-form>
-              </b-card>
-            </div>
-          </b-col>
+    <b-container fluid>
+      <div>
+        <center>
+          <b-card
+            bg-variant
+            text-variant="dark"
+            border-variant="dark"
+            style="max-width: 40rem;"
+            align="left"
+          >
+            <b-form @submit.prevent="login">
+              <center>
+                <p id="msg" v-if="submit_login === 'error'">{{ err_msg }}</p>
+              </center>
+              <b-form-group label="Username:" label-for="username">
+                <b-form-input
+                  id="username"
+                  placeholder="Enter your username"
+                  required
+                  v-model="login_form.username"
+                />
+              </b-form-group>
 
-          <!-- <b-col md="6">
-            <div>
-              <b-card bg-variant text-variant="dark" border-variant="dark">
-                <b-form @submit.prevent="stafflogin">
-                  <center><p id="msg" v-if="submit_stafflogin === 'error'">{{err_msg}}</p></center>
-                  <b-form-group label="Username:" label-for="staff_username">
-                    <b-form-input
-                      id="staff_username"
-                      placeholder="Enter your username"
-                      required
-                      v-model="staff_form.staff_username"
-                    />
-                  </b-form-group>
-
-                  <b-form-group label="Password:" label-for="staff_password">
-                    <b-form-input
-                      id="staff_password"
-                      type="password"
-                      placeholder="Enter your password"
-                      required
-                      v-model="staff_form.staff_password"
-                    />
-                    <br />
-                    <router-link to="/login/resetpassword">Forgot your password?</router-link>
-                  </b-form-group>
-                  <b-button type="submit" variant="success" :disabled="submit_stafflogin === 'ok'">Login</b-button>
-                </b-form>
-              </b-card>
-            </div>
-          </b-col> -->
-        </b-row>
-      </b-container>
-    <!-- </b-jumbotron> -->
+              <b-form-group label="Password:" label-for="password">
+                <b-form-input
+                  id="password"
+                  type="password"
+                  placeholder="Enter your password"
+                  required
+                  v-model="login_form.password"
+                />
+                <br />
+                <router-link to="/login/resetpassword"
+                  >Forgot your password?</router-link
+                >
+              </b-form-group>
+              <b-button
+                type="submit"
+                variant="success"
+                :disabled="submit_login === 'ok'"
+                >Login</b-button
+              >
+            </b-form>
+          </b-card>
+        </center>
+      </div>
+    </b-container>
   </div>
 </template>
 
@@ -104,85 +73,57 @@ export default {
       err_msg: "Invalid Username or Password"
     };
   },
-  
+
   mounted() {
-    axios.get("http://localhost:8085/login").then(response => {
-      this.users = response.data;
+    axios
+      .get("http://localhost:8085/login")
+      .then(response => {
+        this.users = response.data;
         console.log(this.users); //ok
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   },
 
   methods: {
     login() {
       let newLogin = {
-        username:this.login_form.username,
-        password:this.login_form.password,
+        username: this.login_form.username,
+        password: this.login_form.password
       };
       console.log(newLogin);
-      
-      axios.post('http://localhost:8085/login', newLogin).then((resposne) => {
-        console.log(resposne);
 
-        if(this.submit_login = "ok"){
-          this.login_form.user_type = "admin";
-          
-          if(this.users.username == newLogin.username && this.users.password == newLogin.password && this.login_form.user_type == 'admin') {
-            this.$router.push({name: "AdminDashboard"});
-          } 
-          if (this.login_form.user_type == 'staff') {
-            this.$router.push({name: "StaffDashboard"});
+      axios
+        .post("http://localhost:8085/login", newLogin)
+        .then(resposne => {
+          console.log(resposne);
+          if ((this.submit_login = "ok")) {
+            if (
+              this.users[0].username == newLogin.username &&
+              this.users[0].password == newLogin.password
+            ) {
+              this.$router.push({ name: "AdminDashboard" });
+            }
+            if (
+              (this.users[1].username == newLogin.username &&
+                this.users[1].password == newLogin.password) ||
+              (this.users[2].username == newLogin.username &&
+                this.users[2].password == newLogin.password)
+            ) {
+              this.$router.push({ name: "StaffDashboard" });
+            }
           }
-        }
-      })
-      .catch((error) =>{
-        console.log(error);
-        console.log(this.err_msg);
-        this.submit_login = "error";
-      })
+        })
+        .catch(error => {
+          console.log(error);
+          console.log(this.err_msg);
+          this.submit_login = "error";
+        });
     }
-    // adminlogin() {
-    //   let newLogin = {
-    //     username:this.admin_form.admin_username,
-    //     password:this.admin_form.admin_password       
-    //   };
-    //   console.log(newLogin);
-      
-    //   axios.post('http://localhost:8085/adminlogin', newLogin).then((resposne) => {
-    //     console.log(resposne);
-    //     this.submit_adminlogin = "ok";
-    //     this.$router.push({name: "AdminDashboard"});
-    //   })
-    //   .catch((error) =>{
-    //     console.log(error);
-    //     console.log(this.err_msg);
-    //     this.submit_adminlogin = "error";
-    //   })
-    // },
-    // stafflogin() {
-    //   let newLogin = {
-    //     username:this.staff_form.staff_username,
-    //     password:this.staff_form.staff_password      
-    //   };
-    //   console.log(newLogin);
-      
-    //   axios.post('http://localhost:8085/stafflogin', newLogin).then((resposne) => {
-    //     console.log(resposne);
-    //     this.submit_stafflogin = "ok";
-    //     this.$router.push({name: "StaffDashboard"});
-    //   })
-    //   .catch((error) =>{
-    //     console.log(error);
-    //     console.log(this.err_msg);
-    //     this.submit_stafflogin = "error";
-    //   })
-    // }
   }
-}
+};
 </script>
-
 
 <style scoped>
 #form {
@@ -190,8 +131,7 @@ export default {
   padding-bottom: 25px;
 }
 #msg {
-  
   font-size: 15px;
-  color:red;
+  color: red;
 }
 </style>
