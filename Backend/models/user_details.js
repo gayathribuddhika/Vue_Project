fa803ae94b666fde3152c0c4ee2c5e52ccf66273
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-12-28 14:56:38 +0530

    add a new database file

---
+++ Backend/models/user_details.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    
+    profile_image: {
+        type: String,
+        required:[true,"Profile_image is required"]
+    },
+
+    name: {
+        type: String,
+        required:[true,"Name is required"]
+    },
+    
+    email: {
+        type: String,
+        reruired: [true, "Email is required"]
+    }
+}, 
+    {collection: "User"}
+);
+
+module.exports = mongoose.model("User", userSchema);
