31ba76ea59b36c9b18fce54b22bb4176887d0490
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-11-04 15:11:53 +0530

    fix issues in base url

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Axios from 'axios'
+import './axios'
 import BootstrapVue from 'bootstrap-vue'
 import LayoutPlugin from 'bootstrap-vue'
 import NavbarPlugin from 'bootstrap-vue'
@@ -40,7 +40,6 @@ export const bus = new Vue();
 
 new Vue({
   // mode:"history",
-  Axios,
   router,
   render:h => h(App),
 }).$mount('#app')
